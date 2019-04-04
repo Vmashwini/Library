a04ead8a43ab36d367c7af767e394ac59a58e9ea
Vmashwini <46575944+Vmashwini@users.noreply.github.com>
2019-04-04 10:12:46 +0100

    Add files via upload
    this autoload file for mac

--- AutoLoad.php
+++ AutoLoad.php
@@ -1,6 +1,16 @@
 <?php
+namespace LibraryProject\Website;
+
 spl_autoload_register(function($class) 
 {
+    $file_path = "$class.php";
+    $file_path = str_replace('\\','/', $file_path);
     
-    echo "From autoloader ".$class; 
+    echo "FILE PATH IS ::::::::::: ******  ".$file_path."<BR>";
+    
+    if(file_exists($file_path))
+    {
+        include $file_path;
+    }
+    else echo "$class not found";
 });
